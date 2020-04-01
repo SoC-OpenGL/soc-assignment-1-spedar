c681b6640f1c0eeed8579db9730b4a710fa5cd99
spedar <deep.satra@yahoo.in>
2020-04-02 05:08:59 +0530

    Final Code

---
+++ OpenGL-code/problem 2/simple_fs.glsl
@@ -0,0 +1,9 @@
+#version 400
+
+out vec4 frag_colour;
+in vec3 ourColor ;
+
+void main () 
+{
+  frag_colour = vec4 (ourColor, 1.0);
+}
---
+++ OpenGL-code/problem 2/simple_vs.glsl
@@ -0,0 +1,12 @@
+#version 400
+
+layout (location = 0) in vec3 vp;
+layout (location = 1) in vec3 aColor;
+
+out vec3 ourColor;
+
+void main () 
+{
+  gl_Position = vec4 (vp, 1.0);
+  ourColor = aColor ;
+}
